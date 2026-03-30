ed5707da9c1983f01b5bcfc90935c757d27146e6
67160047 <67160047@go.buu.ac.th>
2026-03-31 00:13:54 +0700

    update task4

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FavoritesProvider } from "./context/FavoritesContext";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import PostDetailPage from "./pages/PostDetailPage";
 import ProfilePage from "./pages/ProfilePage";
 import FavoritesPage from "./pages/FavoritesPage";
-import PostDetailPage from "./pages/PostDetailPage";
 
 function App() {
   return (
@@ -13,9 +13,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/posts/:id" element={<PostDetailPage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="/posts/:id" element={<PostDetailPage />} />
         </Routes>
       </BrowserRouter>
     </FavoritesProvider>
